f0f1b2003de6f51d8912d4ad0d1afb2f935fa861
Edgar Marca <matiskay@gmail.com>
2015-05-25 09:52:42 -0300

    Add stack_float scafold

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ all:
 	 gcc -c main.c
 	 gcc -o main stack_string.o main.o
 
+test_stack_float: clean
+	 gcc -c stack_float.c
+	 gcc -c test_stack_float.c
+	 gcc -o test_stack_float stack_float.o test_stack_float.o
+	 ./test_stack_float
+
 test_stack_string: clean
 	 gcc -c stack_string.c
 	 gcc -c test_stack_string.c
@@ -23,7 +29,7 @@ test_functions: clean
 	 gcc -o test_functions functions.o test_functions.o queue_string.o stack_string.o
 	 ./test_functions
 
-test:	 test_stack_string test_functions test_queue_string
+test:	 test_stack_string test_functions test_queue_string test_stack_float
 
 clean:
 	 rm *.out || exit 0
---
+++ test_stack_float.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include "minunit.h"
+#include "stack_float.h"
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\033[0m"
+
+int tests_run = 0;
+
+static char * test_unit() {
+  mu_assert("test_unit failed: \n   error, test_unit 1 != 1", 1 == 1);
+  return 0;
+}
+
+static char * all_tests() {
+  mu_run_test(test_unit);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  char *result = all_tests();
+
+  printf(KMAG "---> Testing stack_float.c \n" RESET);
+
+  if (result != 0) {
+    printf("   -_-_-_-_-_-_-_,------,      o \n");
+    printf("   _-_-_-_-_-_-_-|   /\\_/\\ \n");
+    printf("   -_-_-_-_-_-_-~|__( X .X)  +     + \n");
+    printf("   _-_-_-_-_-_-_- \"\"  \"\" \n");
+    printf(KRED "   ✗ %s \n" RESET, result);
+  }
+  else {
+    printf("   -_-_-_-_-_-_-_,------,      o \n");
+    printf("   _-_-_-_-_-_-_-|   /\\_/\\ \n");
+    printf("   -_-_-_-_-_-_-~|__( ^ .^)  +     + \n");
+    printf("   _-_-_-_-_-_-_-  \"\"  \"\" \n");
+    printf(KGRN "   ✓ ALL TESTS PASSED \n" RESET);
+  }
+
+  printf(KCYN "   Summary:\n   Number of tests run: %d\n" RESET, tests_run);
+  return result != 0;
+}
