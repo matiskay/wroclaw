42972a932b69e7dad7feb814a156356d1060b438
Edgar Marca <matiskay@gmail.com>
2015-06-02 15:07:36 -0300

    Use parameters in main function

--- main.c
+++ main.c
@@ -1,34 +1,50 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "functions.h"
 
 /* Add function signatures here */
 
 int main(int argc, char **argv) {
-  float result;
-  char expression[256] = "(1 + 2) + (3 + 4) + 5";
 
-  QueueString* queue;
-  QueueString* queue_polish;
+  if (argc != 2) {
 
-  queue = queue_string_create();
-  queue_polish = queue_string_create();
+    printf( "Usage: %s <mathematical-expression> \n", argv[0]);
+  } else {
+    QueueString* queue;
+    QueueString* queue_polish;
 
-  queue = parser(expression);
+    char *expression;
+    float result;
+
+    /* TODO: Check if it's a valid expression */
+    expression = argv[1];
+    //    expression = "1 + (2 - (3 * ( 4 / ( 5 + ( 6 - 7 )))))";
+
+    queue = queue_string_create();
+    queue_polish = queue_string_create();
+
+    queue = parser(expression);
+
+    queue_polish = polish_parser(queue);
+
+    printf("Wroclaw: Another Reverse Polish Notation Calculator \n");
+    printf("\n");
+    printf("   Expression: %s \n", expression);
+    printf("   RPN:   ");
+
+    /* Print the Polish Notation */
+    /* TODO: Rename the function to print_polish_notation */
+
+    queue_string_display_inline(queue_polish);
+    printf("\n");
 
 
-  queue_polish = polish_parser(queue);
+    result = polish_evaluation(queue_polish);
 
-  printf("The expression is %s \n", expression);
-  printf("The Polish Notation for this expression is:   ");
-  queue_string_display_inline(queue_polish);
-  printf("\n");
+    printf("   Result: %2.2f \n", result);
 
-  result = polish_evaluation(queue_polish);
+    queue_string_free(queue);
+    queue_string_free(queue_polish);
+  }
 
-  printf("The result is %2.2f \n", result);
-
-  queue_string_free(queue);
-  queue_string_free(queue_polish);
   return 0;
 }
